d1c4172670a43f59c04358649698bf42da05b320
sheeplin <1270610465@qq.com>
2022-07-06 00:23:08 +0800

    chore: add eslint

---
+++ .eslintrc.js
@@ -0,0 +1,10 @@
+module.exports = {
+    extends: [
+        // add more generic rulesets here, such as:
+        "plugin:vue/essential"
+    ],
+    rules: {
+        // override/add rules settings here, such as:
+        "vue/multi-word-component-names": "off",
+    }
+}
