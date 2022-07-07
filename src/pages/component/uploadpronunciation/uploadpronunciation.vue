462376b5de8bca14a5b643821207648d9fd40a27
sheeplin <1270610465@qq.com>
2022-07-07 20:30:44 +0800

    style: refactor template

--- src/pages/component/uploadpronunciation/uploadpronunciation.vue
+++ src/pages/component/uploadpronunciation/uploadpronunciation.vue
@@ -1,19 +1,17 @@
 <template>
   <view>
+    <!--标题-->
     <cu-custom :isBack="true" bgColor="bg-white">
       <view slot="content" class="text-black">发布</view>
     </cu-custom>
+
     <view style="height: 100%; position: absolute; width: 100%">
       <form @submit="submitRecord">
+        <!--表单列表-->
         <block>
           <input class="extend text-bold" disabled value="词语"/>
           <input :value="word" class="fileName" disabled/>
         </block>
-        <!-- <block>
-<input class="extend text-bold" disabled value="录音"></input>
-<input class="extend text-grey" style="width:25%;" disabled value="播放录音" bindtap="playMp3"></input>
-<input class="record" placeholder="{{status==0?'长按开始录音':'松开结束录音'}}" disabled bindlongpress="startRecord" bindtouchend="stopRecord"></input>
-</block> -->
         <block>
           <input class="extend text-bold" disabled value="IPA"/>
           <input :value="ipa" class="fileName" name="ipa" placeholder="请输入该词语的国际音标"/>
@@ -23,7 +21,7 @@
           <input :value="pinyin" class="fileName" name="pinyin" placeholder="请输入该词语的拼音"/>
         </block>
         <view class="flex">
-          <input class="extend text-bold" disabled style="font-size: 34rpx; padding: 13rpx" value="地区"/>
+          <input class="extend text-bold" disabled value="地区"/>
           <picker
               :range="multiArray"
               :value="multiIndex"
@@ -35,30 +33,50 @@
             {{ multiArray[0][multiIndex[0]] }} {{ multiArray[1][multiIndex[1]] }}
           </picker>
         </view>
+
+        <!--录音按钮-->
         <view class="record-btn">
-          <button v-if="source === ''" class="cu-btn icon llg bg-blue shadow margin-bottom-sm" @longpress="startRecord"
-                  @touchend="stopRecord">
-            <text class="cuIcon-voice"></text>
-          </button>
-          <button v-else class="cu-btn icon llg bg-blue shadow margin-bottom-sm" @tap="playMp3">
-            <text class="cuIcon-notificationfill"></text>
-          </button>
-          <view v-if="source === ''" class="text-bold text-xlp">
-            <text v-if="status === 0">长按开始录音</text>
-            <text v-else>松开结束录音</text>
+          <!-- 没有有效的录音文件-->
+          <view v-if="!source">
+            <button
+                class="cu-btn icon llg bg-blue shadow margin-bottom-sm"
+                @longpress="startRecord"
+                @touchend="stopRecord"
+            >
+              <text class="cuIcon-voice"></text>
+            </button>
+            <view class="text-xlp">
+              <text v-if="status === 0">长按开始录音</text>
+              <text v-else>松开结束录音</text>
+            </view>
           </view>
-          <view v-else class="text-bold text-xlp">
-            <text>点击播放录音</text>
+          <!-- 有有效的录音文件-->
+          <view v-else>
+            <button
+                class="cu-btn icon llg bg-blue shadow margin-bottom-sm"
+                @tap="playMp3"
+            >
+              <text class="cuIcon-notificationfill"></text>
+            </button>
+            <view class="text-xlp">
+              <text>点击播放录音</text>
+            </view>
           </view>
         </view>
-        <button v-if="source !== ''" class="cu-btn block bg-red shadow margin" style="width: 90vw; font-size: 32rpx"
-                @tap="reRecord">重新录制
-        </button>
-        <button v-if="source !== ''" class="cu-btn block bg-blue shadow margin" form-type="submit"
-                style="width: 90vw; font-size: 32rpx">确认提交
-        </button>
+
+        <!--有录音文件之后的提交按钮-->
+        <view v-if="source">
+          <button class="cu-btn block bg-red shadow margin" style="width: 90vw;" @tap="reRecord">
+            重新录制
+          </button>
+          <button class="cu-btn block bg-blue shadow margin" form-type="submit" style="width: 90vw;">
+            确认提交
+          </button>
+        </view>
+
       </form>
     </view>
+
   </view>
 </template>
 
@@ -67,7 +85,7 @@ import {uploadFile}      from "@/services/file";
 import request           from "@/utils/request";
 import {counties, towns} from "@/const/location";
 
-const app      = getApp();
+const app = getApp();
 export default {
   data() {
     return {
@@ -238,45 +256,31 @@ export default {
 };
 </script>
 <style>
-page {
-  font-size: 32 rpx;
-}
 
 .fileName {
-  height: 4%;
+  height: 10vw;
   background-color: white;
   padding: 1.8%;
   width: 77.5%;
   margin-left: 2.5%;
   margin-top: 2.5%;
-  border-radius: 10 rpx;
-  display: inline-block;
-}
-
-.record {
-  height: 4%;
-  background-color: white;
-  padding: 1.8%;
-  width: 95%;
-  margin-left: 2.5%;
-  margin-top: 2.5%;
-  border-radius: 10 rpx;
+  border-radius: 2vw;
   display: inline-block;
 }
 
 .extend {
-  height: 4%;
+  height: 10vw;
   background-color: #dbdee2;
   width: 15%;
   margin-left: 2.5%;
   margin-top: 2.5%;
-  border-radius: 10 rpx;
+  border-radius: 2vw;
   display: inline-block;
   text-align: center;
 }
 
 .record-btn {
-  margin-top: 58 rpx;
+  margin-top: 12vw;
   display: flex;
   flex-direction: column;
   align-items: center;
