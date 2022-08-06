6285350332033cc48ab97a7ff9f7041c695adb37
zzy2695s <m18910843053@163.com>
2022-08-06 23:45:29 +0800

    style: fix the format

--- src/pages/about/myarticles/myarticles.vue
+++ src/pages/about/myarticles/myarticles.vue
@@ -1,37 +1,37 @@
 <template>
-    <view>
-        <cu-custom bgColor="bg-white" :isBack="true">
-            <view class="text-black" slot="content">我的文章</view>
-        </cu-custom>
-        <view v-if="publish_articles.length === 0" class="text-lg margin">
-        	<text>这里暂时空空如也~</text>
-        </view>
-        <view v-else class="cu-card article no-card" @tap="article" :data-index="index" v-for="(item, index) in publish_articles" :key="index">
-            <view class="cu-item shadow" style="margin-bottom: 20rpx">
-                <view class="flex justify-between">
-                    <view class="title flex">
-                        <view class="text-cut">{{ item.article.title }}</view>
-                        <view class="cu-tag bg-blue light sm round margin-top-smp margin-left-xs">
-                            <text class="cuIcon-appreciate">{{ item.article.likes }}</text>
-                        </view>
-                    </view>
-                    <text class="cuIcon-attention text-grey margin-top-sm margin-right">{{ item.article.views }}</text>
-                </view>
-                <view class="content">
-                    <view class="desc">
-                        <view class="text-df">
-                            <image class="cu-avatar round ssm" :src="item.author.avatar" mode="aspectFill"></image>
-                            <text :decode="true">&nbsp;{{ item.author.nickname }}&nbsp;&nbsp;</text>
-                            <text class="text-grey">{{ item.article.publish_time }}</text>
-                        </view>
-                        <view class="text-content">{{ item.article.description }}</view>
-                    </view>
-                    <image :src="item.article.cover" mode="aspectFill"></image>
-                </view>
-            </view>
-        </view>
-        <view class="stand-view"></view>
+  <view>
+    <cu-custom bgColor="bg-white" :isBack="true">
+      <view class="text-black" slot="content">我的文章</view>
+    </cu-custom>
+    <view v-if="publish_articles.length === 0" class="text-lg margin">
+      <text>这里暂时空空如也~</text>
     </view>
+    <view v-else class="cu-card article no-card" @tap="article" :data-index="index" v-for="(item, index) in publish_articles" :key="index">
+      <view class="cu-item shadow" style="margin-bottom: 20rpx">
+        <view class="flex justify-between">
+          <view class="title flex">
+            <view class="text-cut">{{ item.article.title }}</view>
+            <view class="cu-tag bg-blue light sm round margin-top-smp margin-left-xs">
+              <text class="cuIcon-appreciate">{{ item.article.likes }}</text>
+            </view>
+          </view>
+          <text class="cuIcon-attention text-grey margin-top-sm margin-right">{{ item.article.views }}</text>
+        </view>
+        <view class="content">
+          <view class="desc">
+            <view class="text-df">
+              <image class="cu-avatar round ssm" :src="item.author.avatar" mode="aspectFill"></image>
+              <text :decode="true">&nbsp;{{ item.author.nickname }}&nbsp;&nbsp;</text>
+              <text class="text-grey">{{ item.article.publish_time }}</text>
+            </view>
+            <view class="text-content">{{ item.article.description }}</view>
+          </view>
+          <image :src="item.article.cover" mode="aspectFill"></image>
+        </view>
+      </view>
+    </view>
+    <view class="stand-view"></view>
+  </view>
 </template>
 
 <script>
