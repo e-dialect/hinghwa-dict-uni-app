b12a73539fc78af13bc1bada5ba83a7473c015e4
sheeplin <1270610465@qq.com>
2022-08-07 01:00:09 +0800

    chore: add "eslint:recommended"

--- .eslintrc.js
+++ .eslintrc.js
@@ -2,9 +2,12 @@ module.exports = {
     extends: [
         // add more generic rulesets here, such as:
         "plugin:vue/recommended",
+        'eslint:recommended',
     ],
     rules: {
         // override/add rules settings here, such as:
         "vue/multi-word-component-names": "off",
+        "no-undef": "off",
+        "no-unused-vars": "off",
     }
 }
