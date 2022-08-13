b69e24644af0156448c3b06918fa6d468281292e
zzy2695s <m18910843053@163.com>
2022-08-13 10:44:13 +0800

    style: fix the format

--- src/components/ArticleComment.vue
+++ src/components/ArticleComment.vue
@@ -31,13 +31,13 @@
     </view>
     <view class="text-content">
       <text
-        v-if="parent-id != comment.parent && comment.parent !== 0"
+        v-if="parentId != comment.parent && comment.parent !== 0"
         class="text-blue"
       >
         @{{ mention }}
       </text>
       <text
-        v-if="parent-id != comment.parent && comment.parent !== 0"
+        v-if="parentId != comment.parent && comment.parent !== 0"
       >
         ：
       </text>
@@ -58,7 +58,7 @@ export default {
       type: Object,
       default: () => ({})
     },
-    parent-id: {
+    parentId: {
       type: String,
       default: ''
     },
