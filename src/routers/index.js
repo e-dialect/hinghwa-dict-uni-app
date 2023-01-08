2511c004cea64f62b4c9b68396540fb2c4c520c3
zzy2695s <m18910843053@163.com>
2023-01-08 21:05:56 +0800

    fix: fix routers page

--- src/routers/index.js
+++ src/routers/index.js
@@ -27,6 +27,52 @@ export function toIndexPage(closeAll = false) {
 }
 
 /**
+ * 前往用户信息页面
+ */
+export function toUserInfoPage() {
+  uni.navigateTo({
+    url: '/pages/about/userinfo/userinfo'
+  });
+}
+
+/**
+ * 前往用户贡献语音页面
+ * @param id 用户id
+ */
+export function toMyRecordsPage(id) {
+  uni.navigateTo({
+    url: '/pages/component/voice/voice?id=' + id
+  })
+}
+
+/**
+ * 前往更改用户昵称页面
+ */
+export function toChangeNicknamePage() {
+  uni.navigateTo({
+    url: '/pages/about/nickname/nickname'
+  })
+}
+
+/**
+ * 前往更改邮箱页面
+ */
+export function toChangeEmailPage() {
+  uni.navigateTo({
+    url: '/pages/about/email/email'
+  })
+}
+
+/**
+ * 前往更改手机号页面
+ */
+export function toChangePhonePage() {
+  uni.navigateTo({
+    url: '/pages/about/phone/phone'
+  })
+}
+
+/**
  * 跳转至词语页面
  * @param id 词语id
  */
