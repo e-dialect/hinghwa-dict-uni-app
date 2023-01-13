c0f20776cc2002d6f1b6510d1ee67b16b55b397f
sheeplin <1270610465@qq.com>
2023-01-13 18:45:27 +0800

    fix: fix main.js

--- src/main.js
+++ src/main.js
@@ -1,4 +1,4 @@
-import Vue, { createSSRApp } from 'vue';
+import Vue from 'vue';
 import App from './App';
 
 // Api函数polyfill（目前为实验版本，如不需要，可删除！）';
@@ -6,11 +6,6 @@ import Polyfill from './polyfill/polyfill';
 
 // 全局mixins，用于实现setData等功能，请勿删除！';
 import Mixin from './polyfill/mixins';
-
-// #ifndef VUE3
-// #endif
-
-// #ifdef VUE3
 
 Polyfill.init();
 
@@ -21,12 +16,3 @@ const app = new Vue({
   ...App,
 });
 app.$mount();
-// eslint-disable-next-line import/prefer-default-export
-export function createApp() {
-  const app2 = createSSRApp(App);
-  app2.mixin(Mixin);
-  return {
-    app: app2,
-  };
-}
-// #endif
