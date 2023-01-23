454bfe358d558101f39dbc073a8c42f7ef3c92be
sheeplin <1270610465@qq.com>
2023-01-23 23:42:04 +0800

    style: fix for eslint

--- src/main.js
+++ src/main.js
@@ -1,11 +1,11 @@
 import { createSSRApp } from 'vue';
 import share from '@/services/shareMessages';
-import App from './App';
 // Api函数polyfill（目前为实验版本，如不需要，可删除！）';
 import Polyfill from '@/polyfill/polyfill';
 
 // 全局mixins，用于实现setData等功能，请勿删除！';
 import Mixin from '@/polyfill/mixins';
+import App from './App';
 
 Polyfill.init();
 
