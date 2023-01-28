c8c81675c867fc788ef151c279b7fa7b17973cf1
sheeplin <1270610465@qq.com>
2023-01-28 19:33:11 +0800

    refactor: optimize

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,18 @@
 import { defineConfig } from 'vite';
 import uni from '@dcloudio/vite-plugin-uni';
+import alias from '@rollup/plugin-alias';
+import { resolve } from 'path';
+
+const projectRootDir = resolve(__dirname);
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     uni(),
+    alias(),
   ],
+  resolve: {
+    alias: {
+      '@': resolve(projectRootDir, 'src'),
+    },
+  },
 });
