b39936a4de871e3b4ba70652a98364d8919e235c
sheeplin <1270610465@qq.com>
2023-01-28 20:08:37 +0800

    fix: use test APIs

--- src/const/urls.js
+++ src/const/urls.js
@@ -1,5 +1,5 @@
 export const COS_URL = 'https://cos.edialect.top/miniprogram';
-const env = import.meta.env.MODE || process.env.NODE_ENV
+const env = 'import.meta.env.MODE' || process.env.NODE_ENV;
 export const BASE_URL = env === 'production'
   ? 'https://api.pxm.edialect.top'
   : 'https://api.pxm.test.edialect.top';
