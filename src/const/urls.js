1e43f299864b280b93c1bf76759df0ad204880b9
sheeplin <1270610465@qq.com>
2023-01-28 19:58:45 +0800

    fix: use test APIs

--- src/const/urls.js
+++ src/const/urls.js
@@ -1,6 +1,7 @@
 export const COS_URL = 'https://cos.edialect.top/miniprogram';
-export const BASE_URL = process.env.NODE_ENV === 'development'
-  ? 'https://api.pxm.test.edialect.top'
-  : 'https://api.pxm.edialect.top';
+const env = import.meta.env.MODE || process.env.NODE_ENV
+export const BASE_URL = env === 'production'
+  ? 'https://api.pxm.edialect.top'
+  : 'https://api.pxm.test.edialect.top';
 
 export const DefaultArticleCover = 'https://cos.edialect.top/website/默认封面.png';
