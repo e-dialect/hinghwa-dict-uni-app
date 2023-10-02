0464d9d2eba48ec8ee208c6d71c9c089667f6144
sheeplin <1270610465@qq.com>
2023-10-02 19:56:10 +0800

    refactor(mails): reorganize files

--- src/routers/mail.js
+++ src/routers/mail.js
@@ -1,9 +1,26 @@
 /**
  * 前往邮件详情界面
  */
-// eslint-disable-next-line import/prefer-default-export
-export function toMailPage(id) {
+export function toMailDetailsPage(id) {
   uni.navigateTo({
-    url: `/pages/tools/mail/mailDetail?id=${id}`,
+    url: `/pages/mails/details?id=${id}`,
+  });
+}
+
+/**
+ * 前往发送邮件界面
+ */
+export function toMailsSendingPage(id) {
+  uni.navigateTo({
+    url: '/pages/mails/send',
+  });
+}
+
+/**
+ * 前往邮件主界面
+ */
+export function toMailsPage(id) {
+  uni.navigateTo({
+    url: '/pages/mails/index',
   });
 }
