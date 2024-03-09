22fa82680f38f9bf5b54aacf4d136c7d8da8b2d7
Shxuuer <72177904+Shxuuer@users.noreply.github.com>
2024-03-09 14:21:48 +0800

    feat: add code style in MarkDown (#192)
    * feat: 添加反引号解析
    
    * style: decrease modification
    
    ---------
    
    Co-authored-by: sheeplin <1270610465@qq.com>

--- src/utils/u-parse/components/wxParseMarkdown.vue
+++ src/utils/u-parse/components/wxParseMarkdown.vue
@@ -1,93 +1,101 @@
 <template>
-  <view>
-    <!--判断是否是标签节点-->
-    <block v-if="node.node === 'element'">
-      <block v-if="node.tag === 'button'">
-        <button size="mini" type="default">
-          <block v-for="(node, index) of node.nodes" :key="index">
-            <wxParseMarkdown :node="node"/>
-          </block>
-        </button>
-      </block>
-
-      <!--li类型-->
-      <block v-else-if="node.tag === 'li'">
-        <view :class="node.classStr" :style="node.styleStr">
-          <block v-for="(node, index) of node.nodes" :key="index">
-            <wxParseMarkdown :node="node"/>
-          </block>
-        </view>
-      </block>
-
-      <!--video类型-->
-      <block v-else-if="node.tag === 'video'">
-        <wx-parse-video :node="node"/>
-      </block>
-
-      <!--audio类型-->
-      <block v-else-if="node.tag === 'audio'">
-        <wx-parse-audio :node="node"/>
-      </block>
-
-      <!--img类型-->
-      <block v-else-if="node.tag === 'img'">
-        <wx-parse-img :node="node"/>
-      </block>
-
-      <!--a类型-->
-      <block v-else-if="node.tag === 'a'">
-        <view :class="node.classStr" :data-href="node.attr.href" :style="node.styleStr" @tap="openURL(node.attr.href)">
-          <block v-for="(node, index) of node.nodes" :key="index">
-            <wxParseMarkdown :node="node"/>
-          </block>
-        </view>
-      </block>
-
-      <!--table类型-->
-      <block v-else-if="node.tag === 'table'">
-        <view :class="node.classStr" :style="node.styleStr" class="table">
-          <block v-for="(node, index) of node.nodes" :key="index">
-            <wxParseMarkdown :node="node"/>
-          </block>
-        </view>
-      </block>
-
-      <!--br类型-->
-      <block v-else-if="node.tag === 'br'">
-        <text>\n</text>
-      </block>
-
-      <!--iframe类型-->
-      <block v-else-if="node.tag === 'iframe'">
-        <view style="width:100%">
-          <i-frame-player :src="node.attr.src"></i-frame-player>
-        </view>
-      </block>
-
-      <!--其他标签-->
-      <block v-else>
-        <view :class="node.classStr" :style="node.styleStr">
-          <block v-for="(node, index) of node.nodes" :key="index">
-            <wxParseMarkdown :node="node"/>
-          </block>
-        </view>
-      </block>
-
+  <!--判断是否是标签节点-->
+  <block v-if="node.node === 'element'">
+    <block v-if="node.tag === 'button'">
+      <button size="mini" type="default">
+        <block v-for="(node, index) of node.nodes" :key="index">
+          <wxParseMarkdown :node="node" />
+        </block>
+      </button>
     </block>
 
-    <!--判断是否是文本节点-->
-    <block v-else-if="node.node === 'text'">{{ node.text }}</block>
-  </view>
+    <!--li类型-->
+    <block v-else-if="node.tag === 'li'">
+      <view :class="node.classStr" :style="node.styleStr">
+        <block v-for="(node, index) of node.nodes" :key="index">
+          <wxParseMarkdown :node="node" />
+        </block>
+      </view>
+    </block>
+
+    <!--video类型-->
+    <block v-else-if="node.tag === 'video'">
+      <wx-parse-video :node="node" />
+    </block>
+
+    <!--audio类型-->
+    <block v-else-if="node.tag === 'audio'">
+      <wx-parse-audio :node="node" />
+    </block>
+
+    <!--img类型-->
+    <block v-else-if="node.tag === 'img'">
+      <wx-parse-img :node="node" />
+    </block>
+
+    <!--a类型-->
+    <block v-else-if="node.tag === 'a'">
+      <view :class="node.classStr" :data-href="node.attr.href" :style="node.styleStr" @tap="openURL(node.attr.href)">
+        <block v-for="(node, index) of node.nodes" :key="index">
+          <wxParseMarkdown :node="node" />
+        </block>
+      </view>
+    </block>
+
+    <!--table类型-->
+    <block v-else-if="node.tag === 'table'">
+      <view :class="node.classStr" :style="node.styleStr" class="table">
+        <block v-for="(node, index) of node.nodes" :key="index">
+          <wxParseMarkdown :node="node" />
+        </block>
+      </view>
+    </block>
+
+    <!--br类型-->
+    <block v-else-if="node.tag === 'br'">
+      <text>\n</text>
+    </block>
+
+    <!--code类型-->
+    <block v-else-if="node.tag === 'code'">
+      <view :class="node.classStr" :style="node.styleStr">
+        <block v-for="(node, index) of node.nodes" :key="index">
+          <wxParseMarkdown :node="node" />
+        </block>
+      </view>
+    </block>
+
+    <!--iframe类型-->
+    <block v-else-if="node.tag === 'iframe'">
+      <view style="width:100%">
+        <i-frame-player :src="node.attr.src" />
+      </view>
+    </block>
+
+    <!--其他标签-->
+    <block v-else>
+      <view :class="node.classStr" :style="node.styleStr">
+        <block v-for="(node, index) of node.nodes" :key="index">
+          <wxParseMarkdown :node="node" />
+        </block>
+      </view>
+    </block>
+  </block>
+
+  <!--判断是否是文本节点-->
+  <block v-else-if="node.node === 'text'" style="display: inline-block">
+    {{ node.text }}
+  </block>
 </template>
 
 <script>
-import wxParseImg      from './wxParseImg';
-import wxParseVideo    from './wxParseVideo';
-import wxParseAudio    from './wxParseAudio';
-import IFramePlayer    from "./IFramePlayer";
+import wxParseImg from './wxParseImg';
+import wxParseVideo from './wxParseVideo';
+import wxParseAudio from './wxParseAudio';
+import IFramePlayer from './IFramePlayer';
 
 export default {
-  name: 'wxParseMarkdown',
+  name: 'WxParseMarkdown',
   props: {
     node: {},
   },
@@ -123,3 +131,13 @@ export default {
   },
 };
 </script>
+
+<style>
+.code {
+  background-color: #eee;
+  padding: 0 5rpx;
+  border-radius: 5%;
+  margin: 3rpx 10rpx;
+  display: inline-block;
+}
+</style>
