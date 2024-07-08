9def2c1d008977a79df7b236fd94d8de58ac37ef
daonan <91860300+daonan233@users.noreply.github.com>
2024-07-08 22:26:08 +0800

    feat(question):fix const urls

--- src/const/urls.js
+++ src/const/urls.js
@@ -3,5 +3,4 @@ export const COS_URL = 'https://cos.edialect.top/miniprogram';
 export const BASE_URL = import.meta.env.MODE === 'production'
   ? 'https://api.pxm.edialect.top'
   : 'https://api.pxm.test.edialect.top';
-
 export const DefaultArticleCover = 'https://cos.edialect.top/website/默认封面.png';
